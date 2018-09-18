7226e10f06a523b1896ae0826d47ddf9285cba7d
OnlyFall(LG_Gram) <34495911+OnlyFall@users.noreply.github.com>
2018-09-18 18:34:17 +0900

    real Drill05_basic code
    -기초 틀

--- Drills/Drill_05/Drill_05.py
+++ Drills/Drill_05/Drill_05.py
@@ -5,7 +5,47 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def move_first_to_second():
+    pass
+
+def move_second_to_third():
+    pass
+
+def move_third_to_fourth():
+    pass
+
+def move_fourth_to_fifth():
+    pass
+
+def move_fifth_to_sixth():
+    pass
+
+def move_sixth_to_seventh():
+    pass
+
+def move_seventh_to_eighth():
+    pass
+
+def move_eighth_to_nine():
+    pass
+
+def move_nine_to_tenth():
+    pass
+
+def move_tenth_to_first():
+    pass
 
 
+while True:
+    move_first_to_second()
+    move_second_to_third()
+    move_third_to_fourth()
+    move_fourth_to_fifth()
+    move_fifth_to_sixth()
+    move_sixth_to_seventh()
+    move_seventh_to_eighth()
+    move_eighth_to_nine()
+    move_nine_to_tenth()
+    move_tenth_to_first()
 
 close_canvas()
