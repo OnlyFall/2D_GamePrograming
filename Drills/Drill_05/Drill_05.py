51bb941b1c550fd400b98b5d624e342d38b6c8e2
OnlyFall(LG_Gram) <34495911+OnlyFall@users.noreply.github.com>
2018-09-18 18:59:05 +0900

    Drill_05(마지막 10번째점 -> 첫번째)
    -귀환 성공

--- Drills/Drill_05/Drill_05.py
+++ Drills/Drill_05/Drill_05.py
@@ -180,7 +180,23 @@ def move_nine_to_tenth():
         delay(0.01)
 
 def move_tenth_to_first():
-    pass
+    startX, startY = 712, 349
+    endX, endY = 203, 535
+    frame = 0
+    moveRangeX = (endX - startX) / 10
+    moveRangeY = (endY - startY) / 10
+
+    while startX > endX:
+        clear_canvas_now()
+        frame = (frame + 1) % 8
+        if moveRangeX > 0:
+            character.clip_draw(frame * 100, 100, 100, 100, startX, startY)
+        elif moveRangeX < 0:
+            character.clip_draw(frame * 100, 0, 100, 100, startX, startY)
+        update_canvas()
+        startX += moveRangeX
+        startY += moveRangeY
+        delay(0.01)
 
 
 while True:
@@ -192,7 +208,7 @@ while True:
    # move_sixth_to_seventh()
    # move_seventh_to_eighth()
    # move_eighth_to_nine()
-    move_nine_to_tenth()
+  #  move_nine_to_tenth()
     move_tenth_to_first()
 
 close_canvas()
