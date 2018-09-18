59a054ab6c1662add3d3ff9e8c42acf4391064ec
OnlyFall(LG_Gram) <34495911+OnlyFall@users.noreply.github.com>
2018-09-18 18:01:57 +0900

    Drill_05(move_from_center_to_right)
    move_from_center_to_right Finished!

--- Drills/Drill_05/Drill_05.py
+++ Drills/Drill_05/Drill_05.py
@@ -7,7 +7,13 @@ character = load_image('character.png')
 
 
 def move_from_center_to_right():
-    pass
+    x, y = 800 // 2, 90
+    while x < 800 - 25:
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y);
+        x += 2
+
 
 def move_up():
     pass
