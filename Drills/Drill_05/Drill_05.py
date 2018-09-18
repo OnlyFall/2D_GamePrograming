109134b1418b81b706a62ff1f02fac7e78905fea
OnlyFall(LG_Gram) <34495911+OnlyFall@users.noreply.github.com>
2018-09-18 19:01:12 +0900

    Drill_05(최종)
    2017182007 김우빈
    Drill_05 완료 커밋 횟수 : 12회 (최종포함)

--- Drills/Drill_05/Drill_05.py
+++ Drills/Drill_05/Drill_05.py
@@ -200,15 +200,15 @@ def move_tenth_to_first():
 
 
 while True:
-  #  move_first_to_second()
-  #  move_second_to_third()
-  #  move_third_to_fourth()
-   # move_fourth_to_fifth()
-   # move_fifth_to_sixth()
-   # move_sixth_to_seventh()
-   # move_seventh_to_eighth()
-   # move_eighth_to_nine()
-  #  move_nine_to_tenth()
+    move_first_to_second()
+    move_second_to_third()
+    move_third_to_fourth()
+    move_fourth_to_fifth()
+    move_fifth_to_sixth()
+    move_sixth_to_seventh()
+    move_seventh_to_eighth()
+    move_eighth_to_nine()
+    move_nine_to_tenth()
     move_tenth_to_first()
 
 close_canvas()
