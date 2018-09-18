e36d31f7d4801ba1077d5047b1edf9a09ca56247
OnlyFall(LG_Gram) <34495911+OnlyFall@users.noreply.github.com>
2018-09-18 18:09:23 +0900

    Drill_05(move_up)
    -올라가는것 완성

--- Drills/Drill_05/Drill_05.py
+++ Drills/Drill_05/Drill_05.py
@@ -16,7 +16,12 @@ def move_from_center_to_right():
 
 
 def move_up():
-    pass
+    x, y = 800 - 25, 50 + 40
+    while y < 600 - 50:
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y);
+        y += 2
 
 def move_left():
     pass
@@ -28,7 +33,7 @@ def move_left_to_center():
     pass
 
 def make_rectangle():
-    move_from_center_to_right()
+   # move_from_center_to_right()
     move_up()
     move_left()
     move_down()
