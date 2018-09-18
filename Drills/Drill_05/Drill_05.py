8475236c23853ee80252a3581a40ad538c06688b
OnlyFall(LG_Gram) <34495911+OnlyFall@users.noreply.github.com>
2018-09-18 17:49:30 +0900

    Drill_05(코드의 기본 구조 완성)
    - Basic code!

--- Drills/Drill_05/Drill_05.py
+++ Drills/Drill_05/Drill_05.py
@@ -5,6 +5,17 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-# fill here
+def make_rectangle():
+    pass
+
+def make_circle():
+    pass
+
+while True:
+    make_rectangle()
+    make_circle()
+
+
+
     
 close_canvas()
