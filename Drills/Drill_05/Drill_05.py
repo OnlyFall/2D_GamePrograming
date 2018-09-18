d3c1f6867e343518c70d7fbc344cf03ef8a816ca
OnlyFall(LG_Gram) <34495911+OnlyFall@users.noreply.github.com>
2018-09-18 17:55:33 +0900

    Drill_05(사각형을 그리는 알고리즘 완성)
    사각형을 그리는 뼈대 완성

--- Drills/Drill_05/Drill_05.py
+++ Drills/Drill_05/Drill_05.py
@@ -5,8 +5,28 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-def make_rectangle():
+
+def move_from_center_to_right():
     pass
+
+def move_up():
+    pass
+
+def move_left():
+    pass
+
+def move_down():
+    pass
+
+def move_left_to_center():
+    pass
+
+def make_rectangle():
+    move_from_center_to_right()
+    move_up()
+    move_left()
+    move_down()
+    move_left_to_center()
 
 def make_circle():
     pass
