4a64ec87c789f05d070596d8af63344deafcca78
OnlyFall(LG_Gram) <34495911+OnlyFall@users.noreply.github.com>
2018-09-20 18:29:56 +0900

    실습 3(마우스 좌표를 따라 이동)
    마우스가 움직이는걸 따라 캐릭터가 따라옴
    (mouse motion)

--- Drills/Drill_06/move_character_with_key.py
+++ Drills/Drill_06/move_character_with_key.py
@@ -4,7 +4,7 @@ from pico2d import *
 def handle_events():
     global running
     global x
-
+    global dir
     events = get_events()
 
     for event in events:
@@ -12,30 +12,44 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                x = x + 10
+                dir += 1
             elif event.key == SDLK_LEFT:
-                x = x - 10
+                dir -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
 
+def moveingCharacter():
+    global frame
+    clear_canvas()
+    frame = (frame + 1) % 8
+    grass.draw(400, 30)
+    if dir > 0:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
+    elif dir < 0:
+        character.clip_draw(frame * 100, 0 * 1, 100, 100, x, 90)
+    elif dir == 0:
+        stopCharacter.draw(x, 90)
+    update_canvas()
 
 open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
+stopCharacter = load_image('character.png')
 
 running = True
 x = 800 // 2
 frame = 0
+dir = 0
 
 while running:
-    clear_canvas()
-    grass.draw(400, 30)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, 90)
-    update_canvas()
-
+    moveingCharacter()
     handle_events()
-    frame = (frame + 1) % 8
-
+    x += dir * 5
     delay(0.05)
 
 close_canvas()
--- Drills/Drill_06/move_character_with_mouse.py
+++ Drills/Drill_06/move_character_with_mouse.py
@@ -4,11 +4,23 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 
 def handle_events():
-    # fill here
-    pass
+   global running
+   global x,y
+
+   events = get_events()
+
+   for event in events:
+       if event.type == SDL_QUIT:
+           running = False
+
+       elif event.type == SDL_MOUSEMOTION:
+           x, y = event.x, KPU_HEIGHT - 1 - event.y
+
+       elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+           running = False
 
 
-# fill here
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
