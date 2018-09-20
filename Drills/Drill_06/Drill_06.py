9cf1600cd7fba678e7ac7729e9a32a20fa8cf0aa
OnlyFall(LG_Gram) <34495911+OnlyFall@users.noreply.github.com>
2018-09-20 19:04:00 +0900

    Drill_06 Finsished
    https://youtu.be/ctZDLIikqCE
    2017182007 김우빈
    
    (마지막 멈춰있을때의 캐릭터 추가)

--- Drills/Drill_06/Drill_06.py
+++ Drills/Drill_06/Drill_06.py
@@ -5,6 +5,7 @@ def handle_events():
     global moveCheck
     global endX, endY
     global mouseX, mouseY
+    global running
     events = get_events()
 
     for event in events:
@@ -12,24 +13,25 @@ def handle_events():
             endX = event.x - 25
             endY = KPU_HEIGHT - 1 - event.y + 26
             moveCheck = True
+            running = True
 
         elif event.type == SDL_MOUSEMOTION:
             mouseX = event.x
             mouseY = KPU_HEIGHT - 1 - event.y
 
 
-def characterDraw():
-    global moveX, moveY
+def characterDraw(moveX):
     global mouseX, mouseY
     global ch_x, ch_y
     global frame
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     frame = (frame + 1) % 8
-    if moveX >= 0:
+
+    if moveX > 0:
         character.clip_draw(frame * 100, 100 * 1, 100, 100, ch_x, ch_y)
     elif moveX < 0:
-        character.clip_draw(frame * 100, 0 , 100, 100, ch_x, ch_y)
+        character.clip_draw(frame * 100, 0, 100, 100, ch_x, ch_y)
     handle_events()
     hand.draw(mouseX, mouseY)
 
@@ -39,6 +41,7 @@ def moveingCharacter():
     global ch_x, ch_y
     global endX, endY
     global moveCheck
+    global running
     moveX = (endX - ch_x) / 100
     moveY = (endY - ch_y) / 100
 
@@ -49,16 +52,26 @@ def moveingCharacter():
         ch_x += moveX
         ch_y += moveY
         count += 1
-        characterDraw()
+        characterDraw(moveX)
         handle_events()
         if moveCheck == True:
             count = 0
             moveX = (endX - ch_x) / 100
             moveY = (endY - ch_y) / 100
             moveCheck = False
-
-
         update_canvas()
+        running = False
+
+def stopCharacter():
+    global ch_x, ch_y
+
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    stop.draw(ch_x, ch_y)
+    handle_events()
+    hand.draw(mouseX, mouseY)
+
+    update_canvas()
 
 
 
@@ -72,7 +85,7 @@ stop = load_image('character.png')
 hand = load_image('hand_arrow.png')
 
 hide_cursor()
-running = True
+running = False
 mouseX, mouseY = KPU_WIDTH / 2, KPU_HEIGHT / 2
 frame = 0
 ch_x, ch_y = KPU_WIDTH / 2, KPU_HEIGHT / 2
@@ -80,8 +93,13 @@ moveX, moveY = KPU_WIDTH / 2, KPU_HEIGHT / 2
 endX, endY = KPU_WIDTH / 2, KPU_HEIGHT / 2
 moveCheck = False
 
-while running:
-    moveingCharacter()
+while True:
+
+    if running == True:
+        moveingCharacter()
+    elif running == False:
+        stopCharacter()
+
 
 close_canvas()
 
