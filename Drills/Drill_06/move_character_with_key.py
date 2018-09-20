cebced4a291482a06dd38b86e5797ce2de01cedb
OnlyFall(LG_Gram) <34495911+OnlyFall@users.noreply.github.com>
2018-09-20 18:04:06 +0900

    실습2 - 키보드 입력 받기
    - 방향키를 이용하여 캐릭터 이동시키기

--- Drills/Drill_06/move_character_with_key.py
+++ Drills/Drill_06/move_character_with_key.py
@@ -2,8 +2,21 @@ from pico2d import *
 
 
 def handle_events():
-    # fill here
-    pass
+    global running
+    global x
+
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                x = x + 10
+            elif event.key == SDLK_LEFT:
+                x = x - 10
+            elif event.key == SDLK_ESCAPE:
+                running = False
 
 
 open_canvas()
