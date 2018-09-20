c942e4efdeedd8f1860368a3165c2d5975259b3b
OnlyFall(LG_Gram) <34495911+OnlyFall@users.noreply.github.com>
2018-09-20 17:51:01 +0900

    실습1 - esc로 프로그램 끄기
    - 윈도우 x키 누르면 꺼지기
    - esc키를 눌러서 프로그램 끄기

--- Drills/Drill_06/character_runs_esc.py
+++ Drills/Drill_06/character_runs_esc.py
@@ -11,8 +11,14 @@ frame = 0
 
 
 def handle_events():
-    # fill here
-    pass
+    global running
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
 
 
 while x < 800 and running:
