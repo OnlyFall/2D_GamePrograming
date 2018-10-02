240bd78c30ea9537b1273fdcdd087221d987e0a7
onlyFall <kwb010712@naver.com>
2018-10-02 19:01:15 +0900

    Drill-07 완성
    2017182007 김우빈
    Drill_07 완료
    총 커밋횟수 : 6회

--- Drills/Drill_07/Drill_07.py
+++ Drills/Drill_07/Drill_07.py
@@ -1,62 +1,12 @@
-import turtle
+
 import random
 from pico2d import *
 
-KPU_WIDTH, KPU_HEIGHT = 1280, 1024
-def stop():
-    turtle.bye()
+WIDTH, HEIGHT = 1280, 1024
 
-
-def prepare_turtle_canvas():
-    turtle.setup(1024, 768)
-    turtle.bgcolor(0.2, 0.2, 0.2)
-    turtle.penup()
-    turtle.hideturtle()
-    turtle.shape('arrow')
-    turtle.shapesize(2)
-    turtle.pensize(5)
-    turtle.color(1, 0, 0)
-    turtle.speed(100)
-    turtle.goto(-500, 0)
-    turtle.pendown()
-    turtle.goto(480, 0)
-    turtle.stamp()
-    turtle.penup()
-    turtle.goto(0, -360)
-    turtle.pendown()
-    turtle.goto(0, 360)
-    turtle.setheading(90)
-    turtle.stamp()
-    turtle.penup()
-    turtle.home()
-
-    turtle.shape('circle')
-    turtle.pensize(1)
-    turtle.color(0, 0, 0)
-    turtle.speed(50)
-
-    turtle.onkey(stop, 'Escape')
-    turtle.listen()
-
-
-def draw_big_point(p):
-    turtle.goto(p)
-    turtle.color(0.8, 0.9, 0)
-    turtle.dot(15)
-    turtle.write('     '+str(p))
-
-
-def draw_point(p):
-    turtle.goto(p)
-    turtle.dot(5, random.random(), random.random(), random.random())
-
-
-def draw_line_basic(p1, p2):
-    pass
 
 frame = 0
-open_canvas(KPU_WIDTH, KPU_HEIGHT)
-kpu_ground = load_image('KPU_GROUND.png')
+open_canvas(WIDTH, HEIGHT)
 character = load_image('animation_sheet.png')
 tempX = 0
 
@@ -88,7 +38,7 @@ def draw_line(p1, p2):
         characterDraw(x, y)
 
 
-#prepare_turtle_canvas()
+
 size = 20
 points = [(random.randint(0, 1200), random.randint(0, 1000)) for i in range(size)]
 n = 1
@@ -97,4 +47,3 @@ while True:
     n = (n + 1) % 20
 
 
-turtle.done()
