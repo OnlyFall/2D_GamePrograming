f0009c851939161f3330cc55dec0db427bb52d15
onlyFall <kwb010712@naver.com>
2018-10-04 18:25:21 +0900

    원래대로는 안오지만 4개점 이동
    돌아가기 구현하러 갑시다!

--- Drills/Drill_08/Drill_08.py
+++ Drills/Drill_08/Drill_08.py
@@ -6,7 +6,37 @@ import random
 def stop():
     turtle.bye()
 
-open_canvas()
+
+def prepare_turtle_canvas():
+    turtle.setup(1024, 768)
+    turtle.bgcolor(0.2, 0.2, 0.2)
+    turtle.penup()
+    turtle.hideturtle()
+    turtle.shape('arrow')
+    turtle.shapesize(2)
+    turtle.pensize(5)
+    turtle.color(1, 0, 0)
+    turtle.speed(100)
+    turtle.goto(-500, 0)
+    turtle.pendown()
+    turtle.goto(480, 0)
+    turtle.stamp()
+    turtle.penup()
+    turtle.goto(0, -360)
+    turtle.pendown()
+    turtle.goto(0, 360)
+    turtle.setheading(90)
+    turtle.stamp()
+    turtle.penup()
+    turtle.home()
+
+    turtle.shape('circle')
+    turtle.pensize(1)
+    turtle.color(0, 0, 0)
+    turtle.speed(50)
+
+    turtle.onkey(stop, 'Escape')
+    turtle.listen()
 
 
 def draw_big_point(p):
@@ -70,10 +100,10 @@ def draw_curve_4_points(p1, p2, p3, p4):
 
 
 
-
-points = [(-350, -100), (-50, 200), (150, -100), (350, 300)]
+prepare_turtle_canvas()
+points = [(-350, 200), (400, 350), (300, -300), (-200, -200)]
 while True:
-    draw_curve_4_points((-350, -100), (-50, 200), (150, -100), (350, 300))
+    draw_curve_4_points(points[0], points[1], points[2],points[3])
 
 
 
