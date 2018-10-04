752bb2794c11a429c5d1953cfc6ab50aff9844f0
onlyFall <kwb010712@naver.com>
2018-10-04 18:26:25 +0900

    첫번째점과 네번째점 이동까지 완성
    3번, 4번 라인과 재계산

--- Drills/Drill_08/Drill_08.py
+++ Drills/Drill_08/Drill_08.py
@@ -97,7 +97,15 @@ def draw_curve_4_points(p1, p2, p3, p4):
         draw_point((x, y))
     draw_point(p4)
 
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p3[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p1[0] + (t ** 3 - t ** 2) * p2[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p1[1] + (t ** 3 - t ** 2) * p2[1]) / 2
+        draw_point((x, y))
 
+    draw_point(p1)
 
 
 prepare_turtle_canvas()
