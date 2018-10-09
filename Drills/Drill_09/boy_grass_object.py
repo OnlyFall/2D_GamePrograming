c4f79e6c3fd8a40136cffa0549043402bbc1e67e
onlyFall <kwb010712@naver.com>
2018-10-09 18:32:11 +0900

    Drill_09기본파일 생성
    - 초기 구현된 친구들 -
    1. 축구단 11명이 오른쪽으로 이동중
    2. 땅이 이미 그려져 있음
    
    - 상기 내용들은 수업중에 구현되었음-

--- Drills/Drill_09/boy_grass_object.py
+++ Drills/Drill_09/boy_grass_object.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import random
 
 # Game object class here
 
@@ -7,7 +8,20 @@ class Grass:
         self.image = load_image('grass.png')
 
     def draw(self):
-        self.image(400, 30)
+        self.image.draw(400, 30)
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0,7)
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100 ,100, self.x, self.y)
 
 def handle_events():
     global running
@@ -19,7 +33,29 @@ def handle_events():
             running = False
 
 # initialization code
+open_canvas()
 
+boy = Boy()
+team = [Boy() for i in range(11)]
+grass = Grass()
+
+running = True
 # game main loop code
+while running:
+    handle_events()
+
+    for boy in team:
+        boy.update()
+
+    clear_canvas()
+    grass.draw()
+
+    for boy in team:
+        boy.draw()
+
+    update_canvas()
+
+    delay(0.05)
 
 # finalization code
+close_canvas()
