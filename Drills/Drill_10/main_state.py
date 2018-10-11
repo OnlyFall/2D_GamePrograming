ed4761294ec2dc731ff6f4a94760b86eb5daf947
onlyFall <kwb010712@naver.com>
2018-10-11 19:08:35 +0900

    뒷배경 보이는 pause!
    배경은 잘보입니다...

--- Drills/Drill_10/main_state.py
+++ Drills/Drill_10/main_state.py
@@ -7,7 +7,7 @@ from pico2d import *
 import game_framework
 import title_state
 import pause_state
-
+import pause_state_second
 
 
 name = "MainState"
@@ -75,7 +75,8 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.change_state(title_state)
         elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
-            game_framework.push_state(pause_state)
+            game_framework.push_state(pause_state_second)
+           # game_framework.push_state(pause_state)
 
 
 def update():
