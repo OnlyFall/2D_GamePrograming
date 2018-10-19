72a0599649d7d2f8ed58bc173b58f5ed96a2b0d8
onlyFall <kwb010712@naver.com>
2018-10-19 22:54:45 +0900

    학교맵 사용을 위한 비트맵 추가
    !

--- FinalTerm/title_state.py
+++ FinalTerm/title_state.py
@@ -17,13 +17,20 @@ def exit():
     del(image)
 
 
+mouseX, mouseY = 0,0
 def handle_events():
-    events = get_events()
+    global mouseX, mouseY
 
+    events = get_events()
     for event in events:
 
         if event.type == SDL_QUIT:
             game_framework.quit()
+
+        if(event.type == SDL_MOUSEMOTION):
+            mouseX = event.x
+            mouseY = event.y
+
         else:
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
                 game_framework.quit()
@@ -35,11 +42,6 @@ def draw():
     clear_canvas()
     image.draw(400, 300)
     update_canvas()
-
-
-
-
-
 
 
 def update():
