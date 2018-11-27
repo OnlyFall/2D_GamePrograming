39dfeb861395fb9ea84c8cc1844b893e145bc83c
OnlyFall <kwb010712@naver.com>
2018-11-27 18:54:40 +0900

    Drill_15완성
    총 커밋수 : 8회
     https://youtu.be/r86YVg9-OJ8
    2017182007 김우빈

--- Drills/Drill_15/ranking_state.py
+++ Drills/Drill_15/ranking_state.py
@@ -11,12 +11,19 @@ import json
 
 Rank = []
 name = "RankingState"
+
 def enter():
     global Rank
-    pass
+    with open('Rank.json', 'r') as f:
+        temp = json.load(f)
+
+        k = temp["Rank"]
+        Rank = k
+
+    Rank.sort(reverse=True)
 
 def exit():
-    pass
+    game_world.clear()
 
 def pause():
     pass
@@ -27,16 +34,38 @@ def resume():
 
 
 def handle_events():
-    pass
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(world_build_state)
 
 
 def update():
     pass
 
 
+font = None
 def draw():
-    pass
+    clear_canvas()
+    global font, Rank
 
+    if font == None:
+        font = load_font('ENCR10B.TTF', 20)
+
+
+    if(len(Rank) < 10):
+        for i in range(len(Rank)):
+            font.draw(100, 500 - 30 * (i + 2), str(i + 1) + '# ( Time:  %3.4f' % (Rank[i]) + ')', (0, 0, 0))
+
+    else:
+        for i in range(10):
+            font.draw(100, 500 - 30 * (i + 2), str(i + 1) + '# ( Time:  %3.4f'  % (Rank[i]) + ')', (0, 0, 0))
+
+
+
+    update_canvas()
 
 
 
