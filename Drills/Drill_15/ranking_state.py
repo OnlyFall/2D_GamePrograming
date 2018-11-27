c94efa5796dd1b9a6dfbcfe076a30b7d5852428d
OnlyFall <kwb010712@naver.com>
2018-11-27 13:02:18 +0900

    랭킹스테이트 추가

---
+++ Drills/Drill_15/ranking_state.py
@@ -0,0 +1,41 @@
+import random
+import json
+import pickle
+import os
+
+from pico2d import *
+import game_framework
+import game_world
+
+import world_build_state
+
+name = "RankingState"
+def enter():
+    pass
+
+def exit():
+    pass
+
+    pass
+
+
+def resume():
+    pass
+
+
+def handle_events():
+    pass
+
+
+def update():
+    pass
+
+
+def draw():
+    pass
+
+
+
+
+
+
